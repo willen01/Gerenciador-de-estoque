c5926a6aee00a2978e5e261c7e9ddbd0210a30a4
willen01 <willen@DESKTOP-R0DTQDB>
2024-01-04 12:27:10 -0300

    Primeiro commit

---
+++ src/main/java/com/willen/meuEstoque/MeuEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.willen.meuEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willen/meuEstoque/MeuEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willen.meuEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
